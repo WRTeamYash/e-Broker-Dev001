21718994d8a8cd6a26195e73345dd9a62dabb2d3
WRTeamYash <yashwteam89@gmail.com>
2023-10-12 11:31:36 +0530

    area converter

--- src/Components/AreaConverter/AreaConverter.jsx
+++ src/Components/AreaConverter/AreaConverter.jsx
@@ -1,95 +1,102 @@
-
-import { translate } from '@/utils';
 import React, { useState } from 'react';
-// import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { RiCloseCircleLine } from 'react-icons/ri';
 
+const unitData = {
+    squareFeet: {
+      name: 'Square Feet',
+      convertFactor: 1,
+    },
+    squareMeter: {
+      name: 'Square Meter',
+      convertFactor: 0.092903,
+    },
+    acre: {
+      name: 'Acre',
+      convertFactor: 0.00002295,
+    },
+    hectare: {
+      name: 'Hectare',
+      convertFactor: 0.000009,
+    },
+    gaj: {
+      name: 'Gaj',
+      convertFactor: 0.112188,
+    },
+    bigha: {
+      name: 'Bigha',
+      convertFactor: 0.000037,
+    },
+    cent: {
+      name: 'Cent',
+      convertFactor: 0.002296,
+    },
+    katha: {
+      name: 'Katha',
+      convertFactor: 0.000735,
+    },
+    guntha: {
+      name: 'Guntha',
+      convertFactor: 0.0009182,
+    },
+  };
+  
 const AreaConverter = ({ isOpen, onClose }) => {
-
     const [value, setValue] = useState('');
     const [fromUnit, setFromUnit] = useState('squareFeet');
     const [toUnit, setToUnit] = useState('squareMeter');
-    const [convertedValue, setConvertedValue] = useState("");
-
-    const unitData = {
-        squareFeet: { name: 'Square Feet', convertFactor: 0.092903 },
-        squareMeter: { name: 'Square Meter', convertFactor: 10.763915 },
-        acre: { name: 'Acre', convertFactor: 0.00002295 },
-        hectare: { name: 'Hectare', convertFactor: 0.000009 },
-        gaj: { name: 'Gaj', convertFactor: 0.112188 },
-        bigha: { name: 'Bigha', convertFactor: 0.000037 },
-        cent: { name: 'Cent', convertFactor: 0.002296 },
-        katha: { name: 'Katha', convertFactor: 0.000735 },
-        guntha: { name: 'Guntha', convertFactor: 0.0009182 },
-    };
-
+    const [convertedValue, setConvertedValue] = useState('');
+  
     const handleValueChange = (event) => {
-        setValue(event.target.value);
+      setValue(event.target.value);
+      setConvertedValue('');
     };
-
+  
     const handleFromUnitChange = (event) => {
-        setFromUnit(event.target.value);
-        setConvertedValue('');
+      setFromUnit(event.target.value);
+      setConvertedValue('');
     };
-
+  
     const handleToUnitChange = (event) => {
-        setToUnit(event.target.value);
-        setConvertedValue('');
+      setToUnit(event.target.value);
+      setConvertedValue('');
     };
-
+  
     const convertValue = () => {
-        const convertFactorFrom = unitData[fromUnit].convertFactor;
-        const convertFactorTo = unitData[toUnit].convertFactor;
-
-        if (convertFactorFrom && convertFactorTo) {
-            const converted =
-                (parseFloat(value) * convertFactorFrom) / convertFactorTo;
-            setConvertedValue(converted.toFixed(2));
-        }
-        // setConvertedValue(true);
+      const fromFactor = unitData[fromUnit].convertFactor;
+      const toFactor = unitData[toUnit].convertFactor;
+  
+      if (fromFactor && toFactor) {
+        const converted = (parseFloat(value) / fromFactor) * toFactor;
+        setConvertedValue(converted);
+      }
     };
-
-
-
+  
 
     return (
-        <div >
-
-            <Modal show={isOpen} onHide={onClose}
-                size="md"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-                className='areaConvert-modal'
-            >
+        <div>
+            <Modal show={isOpen} onHide={onClose} size="md" centered className='areaConvert-modal'>
                 <Modal.Header>
-                    <Modal.Title>{translate("areaConverter")}</Modal.Title>
+                    <Modal.Title>Area Converter</Modal.Title>
                     <RiCloseCircleLine className='close-icon' size={40} onClick={onClose} />
                 </Modal.Header>
                 <Modal.Body>
                     <div className='modal-body-heading'>
-                        <h4>{translate("sqftTosqm")}</h4>
-                        <span>
-                          {translate("enterTheValue&desiredUnit")}
-                        </span>
+                        <h4>Convert Area</h4>
+                        <span>Enter the value and desired units:</span>
                     </div>
                     <div className='area-conrt-fields'>
                         <div className='sq-ft'>
-                            <label>
-                            {translate("form")}
-                            </label>
+                            <label>From:</label>
                             <div className="btn-group" role="group">
-
                                 <input
                                     type="number"
                                     value={value}
                                     onChange={handleValueChange}
                                     placeholder="Enter the value"
                                     id='number-input'
-
                                 />
                                 <select value={fromUnit} onChange={handleFromUnitChange} id='sq-ft-slct'>
-
                                     {Object.keys(unitData).map((unitKey) => (
                                         <option key={unitKey} value={unitKey}>
                                             {unitData[unitKey].name}
@@ -99,43 +106,30 @@ const AreaConverter = ({ isOpen, onClose }) => {
                             </div>
                         </div>
                         <div className='sq-ft'>
-
-                            <label>
-                            {translate("to")}
-                            </label>
+                            <label>To:</label>
                             <div>
-
-                                <select
-                                    // value={toUnit} 
-                                    onChange={handleToUnitChange}
-                                    id='sq-m'>
-                                    <option value="">{translate("chooseValue")}</option>
+                                <select onChange={handleToUnitChange} id='sq-m'>
                                     {Object.keys(unitData).map((unitKey) => (
                                         <option key={unitKey} value={unitKey}>
                                             {unitData[unitKey].name}
                                         </option>
                                     ))}
                                 </select>
-
                             </div>
                         </div>
                     </div>
-
-                    { convertedValue && convertedValue && ( 
+                    {convertedValue && (
                         <div id='show-value'>
                             <span className='convert-value'>
-                                {/* {value} {unitData[fromUnit].name} is equal to  */}
-                                {convertedValue}{''} 
-                                {unitData[toUnit].name}
+                                {value} {unitData[fromUnit].name} = {convertedValue} {unitData[toUnit].name}
                             </span>
                         </div>
                     )}
                 </Modal.Body>
                 <Modal.Footer className='area-footer'>
-                    <button className='convert-button' onClick={convertValue}>{translate("convert")}</button>
-
+                    <button className='convert-button' onClick={convertValue}>Convert</button>
                 </Modal.Footer>
-            </Modal >
+            </Modal>
         </div>
     );
 };

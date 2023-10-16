5c216c69f43c8ea7b17a39ad893da5938ec94719
WRTeamYash <yashwteam89@gmail.com>
2023-10-16 19:01:56 +0530

    finall

--- pages/_app.js
+++ pages/_app.js
@@ -8,37 +8,41 @@ import "../public/css/style.css"
 import "../public/css/responsive.css"
 import 'bootstrap/dist/css/bootstrap.css'
 import 'react-loading-skeleton/dist/skeleton.css'
-// import "../public/css/responsiveStyle.css"
 
 import Head from 'next/head'
 import { Toaster } from 'react-hot-toast'
-import PushNotificationLayout from '@/Components/PushNotificationLayout/PushNotificationLayout'
 
 
-// import Document, { Html, Head, Main, NextScript } from 'next/document';
-const manrope = Manrope({ subsets: ['latin'] })
 
 function MyApp({ Component, pageProps, data }) {
-    // console.log(Component)
+
 
     return (
         <Fragment>
 
             <Head>
-            <title>eBroker- Material Design Next js Template</title>
-                <meta name='description' content="e-Broker – Material Design Next js Admin Dashboard Template – is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5." />
-                <meta name='keywords' content='Material Design, MUI, Admin Template, Next js Admin Template' />
-                <meta name='viewport' content='initial-scale=1, width=device-width' />
-                <link rel='icon' href='/favicon.ico' />
+                <title>eBroker |  Empower Your Real Estate Business</title>
+                <meta name="description" content="Unlock your real estate potential with eBroker - the ultimate solution for your business. Streamline operations, boost efficiency, and succeed in style!" />
+                {/* <meta name="keywords" content="Breaking news,Top stories,Headlines,Current events,Latest news,World news,National news,Local news,Politics,Business,Technology,Entertainment,Sports,Health,Science,Environment,Education,Crime,Immigration,Weather."> */}
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+
+                {/* <!-- Facebook Meta Tags --> */}
+                <meta property="og:url" content="https://newsweb.wrteam.me/" />
+                <meta property="og:type" content="website" />
+                <meta property="og:locale" content="en_GB" />
+                <meta property="og:title" content="eBroker |  Empower Your Real Estate Business" />
+                <meta property="og:description" content="Unlock your real estate potential with eBroker - the ultimate solution for your business. Streamline operations, boost efficiency, and succeed in style!" />
+                <link rel='icon' href='./favicon.ico' />
             </Head>
-                <Provider store={store}>
-                    <PersistGate persistor={persistor}>
+            <Provider store={store}>
+                <PersistGate persistor={persistor}>
 
-                        <Component {...pageProps} data={data} />
+                    <Component {...pageProps} data={data} />
 
-                        <Toaster />
-                    </PersistGate>
-                </Provider>
+                    <Toaster />
+                </PersistGate>
+            </Provider>
         </Fragment>
     );
 }

3e447140e3af5049cf7dbeb55b2f343be5b9668e
Yash <yashwrteam89@gmail.com>
2024-01-11 17:38:12 +0530

    csssss

--- pages/user/index.jsx
+++ pages/user/index.jsx
@@ -1,8 +1,7 @@
 "use client"
 import React from "react";
 import DashboardContent from "../../src/Components/DashboardContent/DashboardContent.jsx";
-import "../../../public/css/style.css";
-import "../../../public/css/responsive.css";
+
 const Index = () => {
     return (
         <>

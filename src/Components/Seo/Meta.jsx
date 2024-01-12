d736841385716e8d8539ebe7c80aeb495f11fc72
Yash <yashwrteam89@gmail.com>
2024-01-12 11:25:10 +0530

    test img in wp

--- src/Components/Seo/Meta.jsx
+++ src/Components/Seo/Meta.jsx
@@ -1,39 +1,42 @@
-import Head from 'next/head';
+import Head from 'next/head'
 
 const Meta = ({ title, description, keywords, ogImage, pathName }) => {
 
   return (
     <Head>
-      <title>{title !== "" && title !== "null" ? title : process.env.NEXT_PUBLIC_META_TITLE}</title>
+      {/* title */}
+      <title>{title ? title : process.env.NEXT_PUBLIC_TITLE}</title>
 
-      {/* Google / Search Engine Tags */}
-      <meta name='name' content={title !== '' ? title : process.env.NEXT_PUBLIC_META_TITLE} />
-      <meta name='description' content={description ? description : process.env.NEXT_PUBLIC_META_DESCRIPTION} />
-      <meta name='keywords' content={keywords ? keywords : process.env.NEXT_PUBLIC_KEYWORDS} />
-      <meta name='image' content={ogImage} />
+      {/*<!-- Google / Search Engine Tags -->*/}
+      <meta name='name' content={title ? title : process.env.NEXT_PUBLIC_TITLE} />
+      <meta name='description' content={description ? description : process.env.NEXT_PUBLIC_DESCRIPTION} />
+      <meta name='keywords' content={keywords ? keywords : process.env.NEXT_PUBLIC_kEYWORDS} />
+      <meta name='image' content={ogImage ? ogImage : '../../assets/Logo_Color.png'} />
 
-      {/* Facebook Meta Tags */}
-      <meta property='og:title' content={title !== "" ? title : process.env.NEXT_PUBLIC_META_TITLE} />
-      <meta property='og:description' content={description ? description : process.env.NEXT_PUBLIC_META_DESCRIPTION} />
-      <meta property='og:image' content={ogImage} />
+      {/*<!-- Facebook Meta Tags -->*/}
+      <meta property='og:title' content={title ? title : process.env.NEXT_PUBLIC_TITLE} />
+      <meta property='og:description' content={description ? description : process.env.NEXT_PUBLIC_DESCRIPTION} />
+      <meta property='og:image' content={ogImage ? ogImage : null} />
       <meta property='og:image:type' content='image/jpg' />
       <meta property='og:image:width' content='1080' />
       <meta property='og:image:height' content='608' />
-      <meta property='og:url' content={pathName} />
+      <meta property='og:url' content={pathName ? pathName : process.env.NEXT_PUBLIC_PATHNAME} />
       <meta property='og:type' content='website' />
 
-      {/* Twitter Meta Tags */}
-      <meta name='twitter:title' content={title !== "" ? title : process.env.NEXT_PUBLIC_META_TITLE} />
-      <meta name='twitter:description' content={description ? description : process.env.NEXT_PUBLIC_META_DESCRIPTION} />
-      <meta name='twitter:image' content={ogImage} />
+      {/*<!-- Twitter Meta Tags -->*/}
+      <meta name='twitter:title' content={title ? title : process.env.NEXT_PUBLIC_TITLE} />
+      <meta name='twitter:description' content={description ? description : process.env.NEXT_PUBLIC_DESCRIPTION} />
+      <meta name='twitter:image' content={ogImage ? ogImage : "../../assets/Logo_Color.png" } />
       <meta name='twitter:card' content='summary_large_image' />
 
-      {/* Robot and Canonical */}
-      <link rel='canonical' href={process.env.NEXT_PUBLIC_WEB_URL} />
+      {/* robot and cononical */}
+      <link rel='canonical' href={`${process.env.NEXT_PUBLIC_WEB_URL}`} />
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       <meta name='robots' content='index, follow,max-snippet:-1,max-video-preview:-1,max-image-preview:large' />
-    </Head>
-  );
-};
 
-export default Meta;
+     
+    </Head>
+  )
+}
+
+export default Meta

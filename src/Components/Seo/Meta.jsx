b489d69cbb3cf34b19a0a6ed307147bc0f44151d
Yash <yashwrteam89@gmail.com>
2024-01-12 11:27:09 +0530

    change in .env

--- src/Components/Seo/Meta.jsx
+++ src/Components/Seo/Meta.jsx
@@ -5,17 +5,17 @@ const Meta = ({ title, description, keywords, ogImage, pathName }) => {
   return (
     <Head>
       {/* title */}
-      <title>{title ? title : process.env.NEXT_PUBLIC_TITLE}</title>
+      <title>{title ? title : process.env.NEXT_PUBLIC_META_TITLE}</title>
 
       {/*<!-- Google / Search Engine Tags -->*/}
-      <meta name='name' content={title ? title : process.env.NEXT_PUBLIC_TITLE} />
-      <meta name='description' content={description ? description : process.env.NEXT_PUBLIC_DESCRIPTION} />
-      <meta name='keywords' content={keywords ? keywords : process.env.NEXT_PUBLIC_kEYWORDS} />
+      <meta name='name' content={title ? title : process.env.NEXT_PUBLIC_META_TITLE} />
+      <meta name='description' content={description ? description : process.env.NEXT_PUBLIC_META_DESCRIPTION} />
+      <meta name='keywords' content={keywords ? keywords : process.env.NEXT_PUBLIC_META_KEYWORD} />
       <meta name='image' content={ogImage ? ogImage : '../../assets/Logo_Color.png'} />
 
       {/*<!-- Facebook Meta Tags -->*/}
-      <meta property='og:title' content={title ? title : process.env.NEXT_PUBLIC_TITLE} />
-      <meta property='og:description' content={description ? description : process.env.NEXT_PUBLIC_DESCRIPTION} />
+      <meta property='og:title' content={title ? title : process.env.NEXT_PUBLIC_META_TITLE} />
+      <meta property='og:description' content={description ? description : process.env.NEXT_PUBLIC_META_DESCRIPTION} />
       <meta property='og:image' content={ogImage ? ogImage : null} />
       <meta property='og:image:type' content='image/jpg' />
       <meta property='og:image:width' content='1080' />
@@ -24,8 +24,8 @@ const Meta = ({ title, description, keywords, ogImage, pathName }) => {
       <meta property='og:type' content='website' />
 
       {/*<!-- Twitter Meta Tags -->*/}
-      <meta name='twitter:title' content={title ? title : process.env.NEXT_PUBLIC_TITLE} />
-      <meta name='twitter:description' content={description ? description : process.env.NEXT_PUBLIC_DESCRIPTION} />
+      <meta name='twitter:title' content={title ? title : process.env.NEXT_PUBLIC_META_TITLE} />
+      <meta name='twitter:description' content={description ? description : process.env.NEXT_PUBLIC_META_DESCRIPTION} />
       <meta name='twitter:image' content={ogImage ? ogImage : "../../assets/Logo_Color.png" } />
       <meta name='twitter:card' content='summary_large_image' />
 

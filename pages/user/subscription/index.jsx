15e8585f18c388eac1883d791881304d5f9568f1
Yash <yashwrteam89@gmail.com>
2024-02-20 12:09:17 +0530

    dynamic ssr false

--- pages/user/subscription/index.jsx
+++ pages/user/subscription/index.jsx
@@ -1,7 +1,9 @@
 "use client"
 
 import React from "react";
-import UserSubScription from '@/Components/User/UserSubScription.jsx'
+// import UserSubScription from '@/Components/User/UserSubScription.jsx'
+const UserSubScription = dynamic(() => import('../../../src/Components/User/UserSubScription.jsx'), { ssr: false })
+
 import Meta from "@/Components/Seo/Meta";
 
 const Index = () => {

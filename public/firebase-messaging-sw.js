333f7cc64e8a1a960e1a45b5267e1548eb1457ba
Yash <yashwrteam89@gmail.com>
2024-03-19 11:41:15 +0530

    eBroker Web 1.1.3

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,13 +3,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compa
 // // Initialize the Firebase app in the service worker by passing the generated config
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD6djyrbNtjhf_ssrww2YkXITUgsS6sYmo",
-    authDomain: "ebroker-wrteam.firebaseapp.com",
-    projectId: "ebroker-wrteam",
-    storageBucket: "ebroker-wrteam.appspot.com",
-    messagingSenderId: "63168540332",
-    appId: "1:63168540332:web:d183e9ca13866ec5623909",
-    measurementId: "G-W05KYC2K8P"
+    apiKey: "xxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxxxxxxxxxx",
+    measurementId: "xxxxxxXXXXXXXXX"
 };
 
 
